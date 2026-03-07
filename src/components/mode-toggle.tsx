efc91173e0ddde071b58b29b2022ff1793a06806
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2026-03-07 23:05:16 +0530

    add

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -3,52 +3,9 @@
 import { Button } from "@/components/ui/button";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
-import * as React from "react";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
-
-  const toggleTheme = (event: React.MouseEvent<HTMLButtonElement>) => {
-    // Check if the View Transition API is supported
-    // @ts-ignore
-    if (!document.startViewTransition) {
-      setTheme(theme === "dark" ? "light" : "dark");
-      return;
-    }
-
-    const x = event.clientX;
-    const y = event.clientY;
-    const endRadius = Math.hypot(
-      Math.max(x, window.innerWidth - x),
-      Math.max(y, window.innerHeight - y),
-    );
-
-    // @ts-ignore
-    const transition = document.startViewTransition(() => {
-      setTheme(theme === "dark" ? "light" : "dark");
-    });
-
-    transition.ready.then(() => {
-      const clipPath = [
-        `circle(0px at ${x}px ${y}px)`,
-        `circle(${endRadius}px at ${x}px ${y}px)`,
-      ];
-
-      document.documentElement.animate(
-        {
-          clipPath: theme === "dark" ? [...clipPath].reverse() : clipPath,
-        },
-        {
-          duration: 400,
-          easing: "ease-in-out",
-          pseudoElement:
-            theme === "dark"
-              ? "::view-transition-old(root)"
-              : "::view-transition-new(root)",
-        },
-      );
-    });
-  };
 
   return (
     <Button
@@ -56,7 +13,7 @@ export function ModeToggle() {
       type="button"
       size="icon"
       className="size-full rounded-full"
-      onClick={toggleTheme}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       <SunIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
       <MoonIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
